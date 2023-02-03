5347d96b43457f805f03bf73d0e5e2a8cc67adfc
Nicolá Domingues <nidomingues@deloitte.pt>
2023-02-03 03:24:14 +0000

    Initial Commit

---
+++ MovieStar/MovieStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieStarApp.swift
+//  MovieStar
+//
+//  Created by Nicolá Domingues on 03/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
